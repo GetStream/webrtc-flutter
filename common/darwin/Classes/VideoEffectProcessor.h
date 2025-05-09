0f1ee2fcace317eef33bf993094f1173f44df9c1
Brazol <brazol@gmail.com>
2025-05-09 11:16:41 +0200

    ios repackaged
    # Conflicts:
    #	common/darwin/Classes/FlutterWebRTCPlugin.h
    #	common/darwin/Classes/FlutterWebRTCPlugin.m
    #	ios/stream_webrtc_flutter.podspec
    #	pubspec.yaml

--- common/darwin/Classes/VideoEffectProcessor.h
+++ common/darwin/Classes/VideoEffectProcessor.h
@@ -1,13 +1,14 @@
-#import <WebRTC/RTCVideoSource.h>
+#import <StreamWebRTC/RTCVideoSource.h>
 
 #import "VideoFrameProcessor.h"
 
-@interface VideoEffectProcessor : NSObject<RTCVideoCapturerDelegate>
+@interface VideoEffectProcessor : NSObject <RTCVideoCapturerDelegate>
 
-@property (nonatomic, strong) NSArray<NSObject<VideoFrameProcessorDelegate> *> *videoFrameProcessors;
-@property (nonatomic, strong) RTCVideoSource *videoSource;
+@property(nonatomic, strong) NSArray<NSObject<VideoFrameProcessorDelegate>*>* videoFrameProcessors;
+@property(nonatomic, strong) RTCVideoSource* videoSource;
 
-- (instancetype)initWithProcessors:(NSArray<NSObject<VideoFrameProcessorDelegate> *> *)videoFrameProcessors
-                       videoSource:(RTCVideoSource *)videoSource;
+- (instancetype)initWithProcessors:
+                    (NSArray<NSObject<VideoFrameProcessorDelegate>*>*)videoFrameProcessors
+                       videoSource:(RTCVideoSource*)videoSource;
 
 @end
