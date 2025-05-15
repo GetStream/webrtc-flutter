de56b3721104957ad505f829c2ff88dedadc3d98
Rene Floor <rene.floor@getstream.io>
2025-05-15 13:43:14 +0200

    update package import

--- lib/src/video_renderer_extension.dart
+++ lib/src/video_renderer_extension.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:stream_webrtc_flutter/stream_webrtc_flutter.dart';
 
 extension VideoRendererExtension on RTCVideoRenderer {
   RTCVideoValue get videoValue => value;
